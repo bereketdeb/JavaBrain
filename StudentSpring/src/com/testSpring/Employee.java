f034fe0dd85670a5171590d510bb3b4167ea1aa6
Bereket Frezghi <bereketdeb@gmail.com>
2017-01-02 15:49:46 -0800

    Git uploading test

---
+++ StudentSpring/src/com/testSpring/Employee.java
@@ -0,0 +1,38 @@
+package com.testSpring;
+
+public class Employee {
+	
+	private String address;
+	private int age;
+	
+	public Employee(int age ){
+		this.age = age;
+		//System.out.print("what");
+	}
+	public Employee(String address ){
+		this.address = address;
+		
+	}
+	
+	public Employee(String address, int age ){
+		this.address = address;
+		this.age = age;
+		
+	}
+	
+	
+	
+	
+	/*public void setAddress(String address){
+		this.address = address;
+	}
+	
+	public String getAddress(){
+		
+		return address;
+	}*/
+	
+	public void getInfo(){
+		System.out.print(address+" "+ age);
+	}
+}
---
+++ StudentSpring/src/com/testSpring/TestClass.java
@@ -0,0 +1,16 @@
+package com.testSpring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Employee emp = (Employee) context.getBean("employee");
+		
+		emp.getInfo();
+	}
+
+}
